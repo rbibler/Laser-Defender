b54b97c574af2c8ea79d23688ede150975408ca5
rbibler <rbibler01@hotmail.com>
2015-12-17 12:53:27 -0500

    Added Enemy Spawner
    And enemy formation

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+	// Use this for initialization
+	void Start () {
+		InstantiateEnemies();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void InstantiateEnemies() {
+		foreach(Transform child in this.transform) {
+			GameObject enemy = Instantiate (enemyPrefab, child.position, 
+			                                new Quaternion(0,0,0,0)) as GameObject;
+			enemy.transform.parent = child;				
+		}
+	}
+}
